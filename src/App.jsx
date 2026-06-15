8a1f84e35056bb8884ccbdb61761049743f97431
sdhuen01-del <sdhuen01@gmail.com>
2026-06-15 15:19:33 +0900

    20260615

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,72 @@
 import { useState } from 'react'
 import './App.css'
-import Modal from './Modal';
+import Modal from './Modal'
 
-function App() {
-  const [title, setTitle] = useState(['경기도','강원도','제주도']);
-  const [clickup, setClickup] = useState(0)
-  const [modal, setModal] = useState(false);
-  
+function App() {  
+  const [title, setTitle] = useState(['경기도', '강원도', '제주도'])
+  const [clickup, setClickup] = useState([0, 0, 0])
+  const [modal, setModal] = useState(false)
+  const [titlenum, setTitlenum] = useState(0)
+  const [print, setPrint] = useState('')
+  const [input, setInput] = useState('')
 
   return (
-    <div className="App">
+    <div className='App'>
       <div className="black-nav">
-        <h4 style={{ color: 'red', fontSize: '20px' }}>React Blog</h4>
+        <h4 style={{color: 'red', fontSize: '20px'}}>React Blog</h4>
       </div>
       
-      <div className="list">
-        <h4 onClick={() => {
-          setModal(!modal)
-          }}>{title[0]} <span onClick={()=>{
-          setClickup(clickup + 1)
-          }}>👍</span> {clickup}</h4>
-        <p>2월 17일 발행</p>
-        <button onClick = {()=>{
-          let copy = [...title]
-          copy[0] = '충청도'
-          setTitle(copy)
-        }}>제목 변경</button>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>6월 12일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>6월 12일 발행</p>
-      </div>
+      {
+        title.map((tit, i) =>
+          <div className="list" key={i}>
+            <h4 onClick={() => {
+              setModal(true)
+              setTitlenum(i)
+            }}>{tit} <span onClick={(e) => {
+              let copy = [...clickup]
+              copy[i] = clickup[i] + 1
+              setClickup(copy)
+              e.stopPropagation()
+            }}>👍</span> {clickup[i]}</h4>
+            <p>6월 12일 발행</p>      
 
-      <div style={{textAlign: 'center', paddingTop: '20px'}}>
+            <button onClick={(e) => {
+              e.stopPropagation()
+
+              let copy = [...title]
+              copy.splice(i, 1)  
+              setTitle(copy)
+
+              let copy2 = [...clickup]
+              copy2.splice(i, 1) 
+              setClickup(copy2)
+
+              setModal(false)   
+            }}>
+              글삭제
+            </button>
+
+
+          </div>
+        )
+      }                
+
+      <div style={{textAlign: 'center', paddingTop:'20px'}}>
+        <input  type='text'   onChange={(e)=> setInput(e.target.value)}   value={input} />
+        <button onClick={()=>{
+          const copy = [...title]
+          copy.unshift(input)
+          setTitle(copy)
+          setInput('')
+
+          const copy2 = [...clickup]
+          copy2.unshift(0)
+          setClickup(copy2)
+        }}>
+          글 추가
+        </button> <br />
+                
+        
         <button onClick={()=>{
           let copy = [...title]
           copy.sort()
@@ -45,9 +75,9 @@ function App() {
       </div>
 
       {
-      modal === true && <Modal/>
-      }
-
+        modal===true && <Modal title={title} color={'skyblue'} titlenum={titlenum} setModal={setModal} setTitle = {setTitle} input={input} setInput={setInput} />
+      }      
+      
     </div>
   )
 }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
-const Modal = () => {
+const Modal = (props) => {
+  const {title, setTitle, color, titlenum, setModal, input, setInput} = props
   return (
-    <div className='modal'>
-        <h4>제목</h4>
+    <div className='modal' style={{backgroundColor: color}}>
+        <h4>제목 : {title[titlenum]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
+
+        <input
+        type="text"
+        onChange={e => setInput(e.target.value)}
+        value={input}
+      />
+
+        <button onClick={()=>{
+          if (input.trim() !== '') {
+            let copy = [...title]
+            copy[titlenum] = input   
+            setTitle(copy)
+            setInput('')
+          }
+        }}>제목 수정</button> <br />
+
+        <button onClick={()=>{
+          setModal(false)}}>닫기</button>
     </div>
   )
 }
